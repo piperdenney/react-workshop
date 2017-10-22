02c1cc6f743732d271f0d3e6d8eb6934a524c91f
Piper Denney <denney.piper@gmail.com>
2017-10-21 23:30:30 -0700

    Add basic component in app. Add my first component in basic component.

--- src/components/MyFirstComponent.js
+++ src/components/MyFirstComponent.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
 
 class MyFirstComponent extends Component {
-
+  render(){
+    return(<h2>
+      This is my first Component!
+    </h2>)
+  }
 }
 
 export default MyFirstComponent;
