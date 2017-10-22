da6f77feb53e53b93676fcd62b0faa01fc631073
Piper Denney <denney.piper@gmail.com>
2017-10-22 00:13:22 -0700

    Add blurb and stats in content.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import Blurb from './Blurb'
+import Stats from './Stats'
 
 class Content extends Component {
   render(){
     return(<div>
       <h2>Home</h2>
+      <Stats location='Seattle' color='Certain shades of blue' sign='Capricorn, scorpio rising, libra moon' />
+      <Blurb />
       </div>)
   }
 }
